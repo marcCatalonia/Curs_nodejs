9e3749107cbe7e07fb19c2af406fe039b95dece6
marc <marc.purgimon@gmail.com>
2024-01-13 12:34:51 +0100

    Working with use(), post() and get() of expressjs

--- app.js
+++ app.js
@@ -19,11 +19,13 @@ app.use('/add-product', (req, res, next)=>{
     res.send('<form action="/product" method="POST"> <input type="text"name="title"> <button type="submit">Add product</button></form>');
 });
 
-app.use('/product', (req, res, next) =>{
+//app.get() and a.post() for filtering those requests
+app.post('/product', (req, res, next) =>{
     console.log(req.body);
     res.redirect('/')
 });
 
+//Use works for all Http methods
 app.use('/', (req, res, next)=>{
     res.send('<h1>Hello from Express.js!</h1>')
     //next(); //Allows the request to continue to the next middleware
