736fbd6f101645f10de173e0d9088580e1ecbc0e
marc <marc.purgimon@gmail.com>
2024-01-13 12:31:16 +0100

    Parsing incoming data with body-parser of expressjs

--- app.js
+++ app.js
@@ -3,33 +3,31 @@ const http = require('http'); //With just the name it will import the module
 
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 
 const app = express();
 
 
-app.use('/', (req, res, next) =>{
-    console.log('This always runs!');
-    next();
-})
+
+//Middleware to parse incoming data using third party library to do so
+//With this we get a Js Object this will only work for som data, not for Files and others
+app.use(bodyParser.urlencoded({extended: false}));
 
 //Function to handle request and response
 app.use('/add-product', (req, res, next)=>{
-    console.log('In another the middleware');
-    res.send('<h1>The add product Page!</h1>')
-    //next(); //Allows the request to continue to the next middleware
+    res.send('<form action="/product" method="POST"> <input type="text"name="title"> <button type="submit">Add product</button></form>');
+});
+
+app.use('/product', (req, res, next) =>{
+    console.log(req.body);
+    res.redirect('/')
 });
 
 app.use('/', (req, res, next)=>{
-    console.log('In another the middleware');
     res.send('<h1>Hello from Express.js!</h1>')
     //next(); //Allows the request to continue to the next middleware
 });
 
 //Calls the create Server and listen
 app.listen(3000);
-//Create a new server
-//Arrow functin with two params request and response
-/* const server = http.createServer(app);
-
-//Make the server listen to calls trough port 3000
-server.listen(3000); */
